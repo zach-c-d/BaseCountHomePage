21e32ea03fcef4af33925e72d79f245fe018577e
Zachary Donsky <zachary.donsky@gmail.com>
2018-07-04 20:26:14 -0400

    finished description section

--- js/index.js
+++ js/index.js
@@ -1,10 +1,21 @@
 // Animate Smooth Scroll
 $('#view-work').on('click', function() {
-  const images = $('#description').position().top;
+  const description = $('#description').position().top;
 
   $('html, body').animate(
     {
-      scrollTop: images
+      scrollTop: description
+    },
+    900
+  );
+});
+
+$('#sign-up').on('click', function() {
+  const signUpFrame = $('#sign-up-frame').position().top;
+
+  $('html, body').animate(
+    {
+      scrollTop: signUpFrame
     },
     900
   );
